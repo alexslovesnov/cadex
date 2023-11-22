dee6e01929e00e165c513931e8b5b5ae02a194fb
alexslovesnov <116576088+alexslovesnov@users.noreply.github.com>
2023-11-22 13:49:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,77 @@
-#pragma once
-
 #include <chrono>
+#include <cstdlib>
 #include <iostream>
-#include <string>
+#include <vector>
 
-class LogDuration {
-public:
-    // заменим имя типа std::chrono::steady_clock
-    // с помощью using для удобства
-    using Clock = std::chrono::steady_clock;
+#include "log_duration.h"
+
+using namespace std;
+
+vector<int> ReverseVector(const vector<int>& source_vector) {
+    vector<int> res;
+    for (int i : source_vector) {
+        res.insert(res.begin(), i);
+    }
+
+    return res;
+}
+
+int CountPops(const vector<int>& source_vector, int begin, int end) {
+    int res = 0;
+
+    for (int i = begin; i < end; ++i) {
+        if (source_vector[i]) {
+            ++res;
+        }
+    }
+
+    return res;
+}
+
+void AppendRandom(vector<int>& v, int n) {
+    for (int i = 0; i < n; ++i) {
+        // получаем случайное число с помощью функции rand.
+        // с помощью (rand() % 2) получим целое число в диапазоне 0..1.
+        // в C++ имеются более современные генераторы случайных чисел,
+        // но в данном уроке не будем их касаться
+        v.push_back(rand() % 2);
+    }
+}
+
+void Operate() {
     
-    LogDuration()
+    vector<int> random_bits;
+
+    // операция << для целых чисел это сдвиг всех бит в двоичной
+    // записи числа. Запишем с её помощью число 2 в степени 17 (131072)
+    static const int N = 1 << 17;
+
+    // заполним вектор случайными числами 0 и 1
     {
+        const LogDuration dur("Append random"s);
+        AppendRandom(random_bits, N);
     }
 
-    LogDuration(std::string operation_name):
-        operation_name_(operation_name)
+    // перевернём вектор задом наперёд
+    vector<int> reversed_bits;
     {
-            
+        const LogDuration dur("Reverse"s);
+        reversed_bits = ReverseVector(random_bits);
     }
 
-    ~LogDuration() {
-        using namespace std::chrono;
-        using namespace std::literals;
+    // посчитаем процент единиц на начальных отрезках вектора
 
-        const auto end_time = Clock::now();
-        const auto dur = end_time - start_time_;
-        std::cerr << operation_name_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+    {
+        const LogDuration dur("Counting"s);
+        for (int i = 1, step = 1; i <= N; i += step, step *= 2) {
+            double rate = CountPops(reversed_bits, 0, i) * 100. / i;
+            cout << "After "s << i << " bits we found "s << rate << "% pops"s << endl;
+        }
     }
 
-private:
-    const Clock::time_point start_time_ = Clock::now();
-    std::string operation_name_;
-};
+}
+
+int main() {
+    const LogDuration total_dur("Total"s);
+    Operate();
+}
